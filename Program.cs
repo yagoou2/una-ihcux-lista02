2f5cd671c3aa809b80974accee5fc252879d41e1
Yago Costa <yagocosta223@gmail.com>
2026-03-26 19:57:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// --- UX / IHC: Saudação e Contexto ---
+Console.Clear();
+Console.WriteLine("========================================");
+Console.WriteLine("   SISTEMA EXPERT: Módulo de Boas-Vindas");
+Console.WriteLine("========================================\n");
+
+// --- Entrada de Dados ---
+Console.Write("Olá, Recruta! Qual é o seu nome de Desenvolvedor(a)? ");
+string nome = Console.ReadLine();
+
+Console.Write($"Prazer, {nome}! Em qual ano você começou a programar? ");
+string entradaAno = Console.ReadLine();
+
+// --- Lógica Simples ---
+int anoInicio = int.Parse(entradaAno);
+int anosDeJornada = DateTime.Now.Year - anoInicio;
+
+// --- Feedback Visual ---
+Console.WriteLine("\n----------------------------------------");
+Console.WriteLine($"STATUS DO PERFIL: {nome.ToUpper()}");
+Console.WriteLine($"TEMPO DE ESTRADA: {anosDeJornada} ano(s) de experiência.");
+Console.WriteLine("----------------------------------------");
+
+Console.WriteLine("\n[Missão Cumprida! Pressione qualquer tecla para encerrar]");
+Console.ReadKey();
